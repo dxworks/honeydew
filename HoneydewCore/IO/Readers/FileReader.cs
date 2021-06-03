bd4b34f191641a2fdf36c586b26b6808967cccc9
Andrei Timar <atimar999@gmail.com>
2021-06-03 16:11:14 +0300

    Added FileReader

---
+++ HoneydewCore/IO/Readers/FileReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace HoneydewCore.IO.Readers
+{
+     public class FileReader : IFileReader
+     {
+          public string ReadFile(string path)
+          {
+               return File.ReadAllText(path);
+          }
+
+          public IList<string> ReadFilePaths(string directoryPath)
+          {
+               try
+               {
+                    return Directory.GetFiles(directoryPath);
+               }
+               catch (Exception e)
+               {
+                    Console.WriteLine(e);
+                    return new List<string>();
+               }
+          }
+     }
+}
---
+++ HoneydewCore/IO/Readers/IFileReader.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace HoneydewCore.IO.Readers
+{
+     public interface IFileReader
+     {
+          string ReadFile(string path);
+
+          IList<string> ReadFilePaths(string directoryPath);
+     }
+}
