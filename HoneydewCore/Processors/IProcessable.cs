f72090cd7a0e4364d8cac41eac2791307b54bd0a
Andrei Timar <atimar999@gmail.com>
2021-06-16 11:02:51 +0300

    Added Processing Chain

---
+++ HoneydewCore/Processors/IProcessable.cs
@@ -0,0 +1,24 @@
+﻿namespace HoneydewCore.Processors
+{
+    public interface IProcessable
+    {
+        public static Processable<T> Of<T>(T value)
+        {
+            return new(value);
+        }
+    }
+
+    public class Processable<T> : IProcessable
+    {
+        public T Value { get; }
+
+        public Processable() : this(default)
+        {
+        }
+
+        public Processable(T value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ HoneydewCore/Processors/ProcessorChain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HoneydewCore.Processors
+{
+    public class ProcessorChain
+    {
+        private IProcessable _processable;
+
+        public ProcessorChain(IProcessable processable)
+        {
+            _processable = processable;
+        }
+
+        public ProcessorChain Process(Func<IProcessable, IProcessable> func)
+        {
+            _processable = func.Invoke(_processable);
+            return this;
+        }
+
+        public ProcessorChain Process<T, TR>(Func<Processable<T>, Processable<TR>> func)
+        {
+            _processable = func.Invoke((Processable<T>) _processable);
+            return this;
+        }
+
+        public IProcessable Finish()
+        {
+            return _processable;
+        }
+
+        public Processable<T> Finish<T>()
+        {
+            return (Processable<T>) _processable;
+        }
+    }
+}
