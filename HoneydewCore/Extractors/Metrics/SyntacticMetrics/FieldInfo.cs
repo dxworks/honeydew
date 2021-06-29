dfea22bfec96d9618d95b1969c2a002b92409329
Andrei Timar <atimar999@gmail.com>
2021-06-29 16:39:42 +0300

    Added Fields Info Metric

---
+++ HoneydewCore/Extractors/Metrics/SyntacticMetrics/FieldInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace HoneydewCore.Extractors.Metrics.SyntacticMetrics
+{
+    public record FieldInfo
+    {
+        public string Name { get; init; }
+        public string Type { get; init; }
+        public string Modifier { get; init; } = "";
+        public string Visibility { get; init; }
+        public bool IsEvent { get; init; }
+    }
+}
---
+++ HoneydewCore/Extractors/Metrics/SyntacticMetrics/FieldsInfoMetric.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace HoneydewCore.Extractors.Metrics.SyntacticMetrics
+{
+    public class FieldsInfoMetric : CSharpMetricExtractor, ISyntacticMetric
+    {
+        public IList<FieldInfo> FieldInfos { get; private set; } = new List<FieldInfo>();
+
+        public override IMetric GetMetric()
+        {
+            return new Metric<IList<FieldInfo>>(FieldInfos);
+        }
+
+        public override string PrettyPrint()
+        {
+            return "Fields Info";
+        }
+
+        public override void VisitEventFieldDeclaration(EventFieldDeclarationSyntax node)
+        {
+            AddFieldInfo(node, true);
+        }
+
+        public override void VisitFieldDeclaration(FieldDeclarationSyntax node)
+        {
+            AddFieldInfo(node, false);
+        }
+
+        private void AddFieldInfo(BaseFieldDeclarationSyntax node, bool isEvent)
+        {
+            var allModifiers = node.Modifiers.ToString();
+            var visibilityModifier = allModifiers;
+            var modifier = "";
+            string[] modifiers = {"readonly", "static", "volatile"};
+            foreach (var m in modifiers)
+            {
+                if (!allModifiers.Contains(m)) continue;
+
+                modifier = m;
+                visibilityModifier = allModifiers.Replace(m, "");
+                visibilityModifier = visibilityModifier.Trim();
+                break;
+            }
+
+            if (string.IsNullOrEmpty(visibilityModifier))
+            {
+                visibilityModifier = "private";
+            }
+
+            foreach (var variable in node.Declaration.Variables)
+            {
+                FieldInfos.Add(new FieldInfo
+                {
+                    Visibility = visibilityModifier,
+                    Modifier = modifier,
+                    IsEvent = isEvent,
+                    Type = node.Declaration.Type.ToString(),
+                    Name = variable.Identifier.ToString(),
+                });
+            }
+        }
+    }
+}
