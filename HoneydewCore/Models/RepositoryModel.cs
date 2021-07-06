3de06c4c5c94c94c48ed4b2696fe77689a4c9e18
Andrei Timar <atimar999@gmail.com>
2021-07-06 16:24:52 +0300

    Modified SolutionModelJsonExporterTests.cs to RepositoryModelToClassRelationsProcessor.cs

--- HoneydewCore/Models/RepositoryModel.cs
+++ HoneydewCore/Models/RepositoryModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using HoneydewCore.IO.Writers.Exporters;
 
 namespace HoneydewCore.Models
@@ -15,6 +16,23 @@ namespace HoneydewCore.Models
             }
 
             return string.Empty;
+        }
+
+        public IEnumerable<ClassModel> GetEnumerable()
+        {
+            foreach (var solutionModel in Solutions)
+            {
+                foreach (var projectModel in solutionModel.Projects)
+                {
+                    foreach (var (_, namespaceModel) in projectModel.Namespaces)
+                    {
+                        foreach (var classModel in namespaceModel.ClassModels)
+                        {
+                            yield return classModel;
+                        }
+                    }
+                }
+            }
         }
     }
 }
