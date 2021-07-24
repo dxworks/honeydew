544317569b85f6d4aa000ae84661522e55d54448
Andrei Timar <atimar999@gmail.com>
2021-07-24 11:12:33 +0300

    Relocated Metric Prettier and Metric Relations Provider

---
+++ HoneydewExtractors/Core/MetricPrettier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using HoneydewCore.ModelRepresentations;
+using HoneydewExtractors.Core.Metrics;
+
+namespace HoneydewExtractors.Core
+{
+    public class MetricPrettier : IMetricPrettier
+    {
+        public string Pretty(string metricFullName)
+        {
+            if (string.IsNullOrWhiteSpace(metricFullName))
+            {
+                return "";
+            }
+
+            var type = Type.GetType(metricFullName);
+            if (type == null || !typeof(IMetric).IsAssignableFrom(type))
+            {
+                return metricFullName;
+            }
+
+            var metricExtractor = (IMetric) Activator.CreateInstance(type);
+
+            return metricExtractor == null ? metricFullName : metricExtractor.PrettyPrint();
+        }
+    }
+}
