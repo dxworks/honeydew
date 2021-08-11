08d7c17c3d4ec13311d04d7185158c09fe9fee37
Andrei Timar <atimar999@gmail.com>
2021-08-11 14:19:23 +0300

    Exported CyclomaticComplexityPerFileRepresentation to json

---
+++ HoneydewCore/IO/Writers/Exporters/JsonCyclomaticComplexityPerFileRepresentationExporter.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+using HoneydewCore.ModelRepresentations;
+using HoneydewModels.Exporters;
+
+namespace HoneydewCore.IO.Writers.Exporters
+{
+    public class
+        JsonCyclomaticComplexityPerFileRepresentationExporter : IModelExporter<
+            CyclomaticComplexityPerFileRepresentation>
+    {
+        public string Export(CyclomaticComplexityPerFileRepresentation model)
+        {
+            return JsonSerializer.Serialize(model);
+        }
+    }
+}
