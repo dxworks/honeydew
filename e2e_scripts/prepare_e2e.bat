5b9173bdee57afdde93dae6efe23d6b78b34be71
Andrei Timar <atimar999@gmail.com>
2021-08-12 16:37:38 +0300

    Added copy scripts for e2e test

---
+++ e2e_scripts/prepare_e2e.bat
@@ -0,0 +1,50 @@
+@ECHO OFF
+REM the script will take the desired files from the provided source folder and copy it to the destination folder 
+REM if no destination folder is provided, then the destination folder will be '../e2e_results'
+
+IF [%1] == [] (
+	ECHO "Usage %0 <source_results_folder> [destination_folder]"
+	EXIT 1
+)
+
+IF NOT [%3] == [] (
+	ECHO "Usage %0 <source_results_folder> [destination_folder]"
+	EXIT 1
+)
+
+IF [%2] == [] (
+	SET dst_folder=..\e2e_results
+)
+IF NOT [%2] == [] (
+	SET dst_folder=%~2
+)
+
+SET src_folder=%~1
+
+IF NOT EXIST %dst_folder% (
+	mkdir %dst_folder%
+)
+
+CALL :copy_file honeydew.csv
+CALL :copy_file honeydew_intermediate.csv
+CALL :copy_file honeydew.json
+CALL :copy_file honeydew_intermediate.json
+CALL :copy_file honeydew_cyclomatic.json
+CALL :copy_file honeydew_cyclomatic_intermediate.json
+CALL :copy_file honeydew_namespaces.json
+
+
+EXIT /B 0
+ 
+:copy_file
+
+IF NOT EXIST "%src_folder%/%1" ( 
+	ECHO File %1 does not exist in %src_folder%
+	EXIT 2
+)
+
+echo Copying "%src_folder%/%1" to "%dst_folder%/%1"
+ 
+COPY "%src_folder%\%1" %dst_folder%
+
+EXIT /B 0
---
+++ e2e_scripts/prepare_e2e.sh
@@ -0,0 +1,34 @@
+# the script will take the desired files from the provided source folder and copy it to the destination folder 
+# if no destination folder is provided, then the destination folder will be '../e2e_results'
+if test $# -eq 1
+then
+	dst_folder="../e2e_results"
+elif test $# -eq 2
+then
+	dst_folder="$2"
+else
+  echo "Usage $0 <source_results_folder> [destination_folder]"; exit 1
+fi
+
+src_folder="$1"
+
+mkdir -p $dst_folder
+
+copy_file () {
+	if ! test -f "$src_folder/$1"
+	then 
+		echo "File $1 does not exist in $src_folder"; exit 2
+	fi
+
+	echo "Copying $src_folder/$1 to $dst_folder/$1"
+	
+	cp -R "$src_folder/$1" "$dst_folder/$1"
+}
+
+copy_file "honeydew.csv"
+copy_file "honeydew_intermediate.csv"
+copy_file "honeydew.json"
+copy_file "honeydew_intermediate.json"
+copy_file "honeydew_cyclomatic.json"
+copy_file "honeydew_cyclomatic_intermediate.json"
+copy_file "honeydew_namespaces.json"
