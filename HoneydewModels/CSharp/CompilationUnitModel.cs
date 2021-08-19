09c642ffe07db59bd8945f3567e8deb9b0ca6d52
Andrei Timar <atimar999@gmail.com>
2021-08-19 15:17:13 +0300

    Added Compilation Unit Model and Type

---
+++ HoneydewModels/CSharp/CompilationUnitModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using HoneydewModels.Types;
+
+namespace HoneydewModels.CSharp
+{
+    public class CompilationUnitModel : ICompilationUnitType
+    {
+        public IList<IClassType> ClassTypes { get; set; } = new List<IClassType>();
+
+        public string FilePath { get; set; }
+
+        public IList<IImportType> Imports { get; set; } = new List<IImportType>();
+    }
+}
