33c7b579ac4a4dacf0894cc2fcf45604525bbb8f
Andrei Timar <atimar999@gmail.com>
2021-08-19 16:08:49 +0300

    Added Property Visitors

---
+++ HoneydewExtractors/Core/Metrics/Visitors/Properties/ICSharpPropertyVisitor.cs
@@ -0,0 +1,10 @@
+﻿using HoneydewModels.Types;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace HoneydewExtractors.Core.Metrics.Visitors.Properties
+{
+    public interface ICSharpPropertyVisitor : IPropertyVisitor, ICSharpTypeVisitor,
+        IVisitorType<BasePropertyDeclarationSyntax, IPropertyType>
+    {
+    }
+}
