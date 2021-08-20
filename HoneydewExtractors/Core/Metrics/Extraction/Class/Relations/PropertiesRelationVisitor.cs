3b1e123169b84750e4f743caab228aed1df8a501
Andrei Timar <atimar999@gmail.com>
2021-08-20 09:02:29 +0300

    Updated CSharpPropertiesRelationMetric

---
+++ HoneydewExtractors/Core/Metrics/Extraction/Class/Relations/PropertiesRelationVisitor.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace HoneydewExtractors.Core.Metrics.Extraction.Class.Relations
+{
+    public class PropertiesRelationVisitor  : RelationMetricVisitor
+    {
+        public PropertiesRelationVisitor(IRelationMetricHolder metricHolder) : base(metricHolder)
+        {
+        }
+
+        public override string PrettyPrint()
+        {
+            return "Properties Dependency";
+        }
+
+        protected override void AddDependencies(string className, BaseTypeDeclarationSyntax syntaxNode)
+        {
+            foreach (var fieldDeclarationSyntax in syntaxNode.DescendantNodes().OfType<PropertyDeclarationSyntax>())
+            {
+                MetricHolder.Add(className,
+                    InheritedSemanticModel.GetFullName(fieldDeclarationSyntax.Type));
+            }
+
+            foreach (var eventFieldDeclarationSyntax in syntaxNode.DescendantNodes()
+                .OfType<EventDeclarationSyntax>())
+            {
+                MetricHolder.Add(className,
+                    InheritedSemanticModel.GetFullName(eventFieldDeclarationSyntax.Type));
+            }        }
+    }
+}
