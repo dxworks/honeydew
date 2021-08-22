4fbe43c4f1d286f2e90f0ba8f128115488f9a311
Andrei Timar <atimar999@gmail.com>
2021-08-22 22:19:44 +0300

    Added Visitor For ITypeVisitor

---
+++ HoneydewExtractors/Core/Metrics/Visitors/ExtractionModelsSetterVisitor.cs
@@ -0,0 +1,28 @@
+﻿using HoneydewExtractors.CSharp.Metrics;
+
+namespace HoneydewExtractors.Core.Metrics.Visitors
+{
+    public class ExtractionModelsSetterVisitor : IVisitor
+    {
+        private readonly CSharpSyntacticModel _syntacticModel;
+        private readonly CSharpSemanticModel _semanticModel;
+
+        public ExtractionModelsSetterVisitor(CSharpSyntacticModel syntacticModel, CSharpSemanticModel semanticModel)
+        {
+            _syntacticModel = syntacticModel;
+            _semanticModel = semanticModel;
+        }
+
+        public void Visit(ITypeVisitor visitor)
+        {
+            if (visitor is IExtractionDependencyVisitor<CSharpSyntacticModel, CSharpSemanticModel>
+                extractionVisitor)
+            {
+                extractionVisitor.SetSyntacticModel(_syntacticModel);
+                extractionVisitor.SetSemanticModel(_semanticModel);
+            }
+            
+            visitor.Accept(this);
+        }
+    }
+}
