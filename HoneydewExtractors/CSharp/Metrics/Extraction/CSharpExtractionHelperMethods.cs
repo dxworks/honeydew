1a8f16281ab47aeb15a4c782838df0c4dfd83c38
Andrei Timar <atimar999@gmail.com>
2021-08-25 14:42:53 +0300

    Added Null check for ExpressionSyntax in GetFullName

--- HoneydewExtractors/CSharp/Metrics/Extraction/CSharpExtractionHelperMethods.cs
+++ HoneydewExtractors/CSharp/Metrics/Extraction/CSharpExtractionHelperMethods.cs
@@ -176,7 +176,13 @@ namespace HoneydewExtractors.CSharp.Metrics.Extraction
 
         public string GetFullName(ExpressionSyntax expressionSyntax)
         {
-            var symbolInfo = ModelExtensions.GetSymbolInfo(_semanticModel, expressionSyntax);
+            if (expressionSyntax == null)
+            {
+                return "";
+            }
+
+            var symbolInfo = _semanticModel.GetSymbolInfo(expressionSyntax);
+
             switch (symbolInfo.Symbol)
             {
                 case IPropertySymbol propertySymbol:
