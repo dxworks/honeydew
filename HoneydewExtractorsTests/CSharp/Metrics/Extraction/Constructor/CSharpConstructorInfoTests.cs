3bfa43ab431eb2c2d04d1db46cb1fd778606cc9e
Andrei Timar <atimar999@gmail.com>
2021-09-07 09:53:30 +0300

    HON-88
    Static Constructor Extraction

---
+++ HoneydewExtractorsTests/CSharp/Metrics/Extraction/Constructor/CSharpConstructorInfoTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using HoneydewExtractors.Core.Metrics.Visitors;
+using HoneydewExtractors.Core.Metrics.Visitors.Classes;
+using HoneydewExtractors.Core.Metrics.Visitors.Constructors;
+using HoneydewExtractors.CSharp.Metrics;
+using HoneydewExtractors.CSharp.Metrics.Extraction.Class;
+using HoneydewExtractors.CSharp.Metrics.Extraction.CompilationUnit;
+using HoneydewExtractors.CSharp.Metrics.Extraction.Constructor;
+using HoneydewModels.CSharp;
+using Xunit;
+
+namespace HoneydewExtractorsTests.CSharp.Metrics.Extraction.Constructor
+{
+    public class CSharpConstructorInfoTests
+    {
+        private readonly CSharpFactExtractor _factExtractor;
+
+        public CSharpConstructorInfoTests()
+        {
+            var compositeVisitor = new CompositeVisitor();
+
+            compositeVisitor.Add(new ClassSetterCompilationUnitVisitor(new List<IClassVisitor>
+            {
+                new BaseInfoClassVisitor(),
+                new ConstructorSetterClassVisitor(new List<ICSharpConstructorVisitor>
+                {
+                    new ConstructorInfoVisitor(),
+                })
+            }));
+
+            _factExtractor = new CSharpFactExtractor(new CSharpSyntacticModelCreator(),
+                new CSharpSemanticModelCreator(new CSharpCompilationMaker()), compositeVisitor);
+        }
+
+        [Theory]
+        [InlineData("class")]
+        [InlineData("record")]
+        [InlineData("struct")]
+        [InlineData("interface")]
+        public void Extract_ShouldExtractStaticConstructor_WhenProvidedWithDifferentClassType(string classType)
+        {
+            var fileContent = $@"namespace Namespace1
+{{
+    public {classType} Class1 
+    {{
+        static Class1() {{}}       
+    }}
+}}";
+
+            var classTypes = _factExtractor.Extract(fileContent).ClassTypes;
+
+            var classModel = (ClassModel)classTypes[0];
+
+            Assert.Equal(1, classModel.Constructors.Count);
+
+            Assert.Equal("Class1", classModel.Constructors[0].Name);
+            Assert.Equal("static", classModel.Constructors[0].Modifier);
+            Assert.Equal("", classModel.Constructors[0].AccessModifier);
+            Assert.Empty(classModel.Constructors[0].ParameterTypes);
+        }
+
+    }
+}
