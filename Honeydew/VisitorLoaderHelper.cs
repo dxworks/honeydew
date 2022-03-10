fa2ca141cfa6e260c7d98607e092e1ed18dd7034
Andrei Timar <atimar999@gmail.com>
2022-03-10 11:08:11 +0200

    HON-56
    Removed Unused FullNameModelProcessor and Unused Metrics

--- Honeydew/VisitorLoaderHelper.cs
+++ Honeydew/VisitorLoaderHelper.cs
@@ -32,60 +32,52 @@ using HoneydewExtractors.CSharp.Metrics.Extraction.Property;
 using HoneydewExtractors.CSharp.Metrics.Visitors.Method;
 using HoneydewExtractors.CSharp.Metrics.Visitors.Method.LocalFunctions;
 
-namespace Honeydew
+namespace Honeydew;
+
+internal static class VisitorLoaderHelper
 {
-    internal static class VisitorLoaderHelper
+    public static ICompositeVisitor LoadVisitors(IRelationMetricHolder relationMetricHolder, ILogger logger)
     {
-        public static ICompositeVisitor LoadVisitors(IRelationMetricHolder relationMetricHolder, ILogger logger)
+        var linesOfCodeVisitor = new LinesOfCodeVisitor();
+
+        var attributeSetterVisitor = new AttributeSetterVisitor(new List<IAttributeVisitor>
         {
-            var linesOfCodeVisitor = new LinesOfCodeVisitor();
+            new AttributeInfoVisitor()
+        });
 
-            var attributeSetterVisitor = new AttributeSetterVisitor(new List<IAttributeVisitor>
+        var calledMethodSetterVisitor = new CalledMethodSetterVisitor(new List<ICSharpMethodCallVisitor>
+        {
+            new MethodCallInfoVisitor()
+        });
+
+        var accessedFieldsSetterVisitor = new AccessedFieldsSetterVisitor(new List<ICSharpAccessedFieldsVisitor>
+        {
+            new AccessFieldVisitor()
+        });
+
+        var parameterSetterVisitor = new ParameterSetterVisitor(new List<IParameterVisitor>
+        {
+            new ParameterInfoVisitor(),
+            attributeSetterVisitor
+        });
+
+        var genericParameterSetterVisitor = new GenericParameterSetterVisitor(new List<IGenericParameterVisitor>
+        {
+            new GenericParameterInfoVisitor(),
+            attributeSetterVisitor
+        });
+
+        var localVariablesTypeSetterVisitor = new LocalVariablesTypeSetterVisitor(new List<ILocalVariablesVisitor>
+        {
+            new LocalVariableInfoVisitor()
+        });
+
+        var gotoInfoVisitor = new GotoStatementVisitor();
+
+        var localFunctionsSetterClassVisitor = new LocalFunctionsSetterClassVisitor(new List<ILocalFunctionVisitor>
+        {
+            new LocalFunctionInfoVisitor(new List<ILocalFunctionVisitor>
             {
-                new AttributeInfoVisitor()
-            });
-
-            var calledMethodSetterVisitor = new CalledMethodSetterVisitor(new List<ICSharpMethodCallVisitor>
-            {
-                new MethodCallInfoVisitor()
-            });
-
-            var accessedFieldsSetterVisitor = new AccessedFieldsSetterVisitor(new List<ICSharpAccessedFieldsVisitor>
-            {
-                new AccessFieldVisitor()
-            });
-
-            var parameterSetterVisitor = new ParameterSetterVisitor(new List<IParameterVisitor>
-            {
-                new ParameterInfoVisitor(),
-                attributeSetterVisitor
-            });
-
-            var genericParameterSetterVisitor = new GenericParameterSetterVisitor(new List<IGenericParameterVisitor>
-            {
-                new GenericParameterInfoVisitor(),
-                attributeSetterVisitor
-            });
-
-            var localVariablesTypeSetterVisitor = new LocalVariablesTypeSetterVisitor(new List<ILocalVariablesVisitor>
-            {
-                new LocalVariableInfoVisitor()
-            });
-
-            var gotoInfoVisitor = new GotoStatementVisitor();
-
-            var localFunctionsSetterClassVisitor = new LocalFunctionsSetterClassVisitor(new List<ILocalFunctionVisitor>
-            {
-                new LocalFunctionInfoVisitor(new List<ILocalFunctionVisitor>
-                {
-                    calledMethodSetterVisitor,
-                    linesOfCodeVisitor,
-                    parameterSetterVisitor,
-                    localVariablesTypeSetterVisitor,
-                    genericParameterSetterVisitor,
-                    accessedFieldsSetterVisitor,
-                    gotoInfoVisitor,
-                }),
                 calledMethodSetterVisitor,
                 linesOfCodeVisitor,
                 parameterSetterVisitor,
@@ -93,123 +85,129 @@ namespace Honeydew
                 genericParameterSetterVisitor,
                 accessedFieldsSetterVisitor,
                 gotoInfoVisitor,
-            });
+            }),
+            calledMethodSetterVisitor,
+            linesOfCodeVisitor,
+            parameterSetterVisitor,
+            localVariablesTypeSetterVisitor,
+            genericParameterSetterVisitor,
+            accessedFieldsSetterVisitor,
+            gotoInfoVisitor,
+        });
 
-            var methodInfoVisitor = new MethodInfoVisitor();
+        var methodInfoVisitor = new MethodInfoVisitor();
 
-            var methodVisitors = new List<ICSharpMethodVisitor>
-            {
-                methodInfoVisitor,
-                linesOfCodeVisitor,
-                calledMethodSetterVisitor,
-                localFunctionsSetterClassVisitor,
-                attributeSetterVisitor,
-                parameterSetterVisitor,
-                localVariablesTypeSetterVisitor,
-                genericParameterSetterVisitor,
-                accessedFieldsSetterVisitor,
-                gotoInfoVisitor,
-            };
+        var methodVisitors = new List<ICSharpMethodVisitor>
+        {
+            methodInfoVisitor,
+            linesOfCodeVisitor,
+            calledMethodSetterVisitor,
+            localFunctionsSetterClassVisitor,
+            attributeSetterVisitor,
+            parameterSetterVisitor,
+            localVariablesTypeSetterVisitor,
+            genericParameterSetterVisitor,
+            accessedFieldsSetterVisitor,
+            gotoInfoVisitor,
+        };
 
-            var constructorVisitors = new List<ICSharpConstructorVisitor>
-            {
-                new ConstructorInfoVisitor(),
-                linesOfCodeVisitor,
-                calledMethodSetterVisitor,
-                new ConstructorCallsVisitor(),
-                localFunctionsSetterClassVisitor,
-                attributeSetterVisitor,
-                parameterSetterVisitor,
-                localVariablesTypeSetterVisitor,
-                accessedFieldsSetterVisitor,
-                gotoInfoVisitor,
-            };
+        var constructorVisitors = new List<ICSharpConstructorVisitor>
+        {
+            new ConstructorInfoVisitor(),
+            linesOfCodeVisitor,
+            calledMethodSetterVisitor,
+            new ConstructorCallsVisitor(),
+            localFunctionsSetterClassVisitor,
+            attributeSetterVisitor,
+            parameterSetterVisitor,
+            localVariablesTypeSetterVisitor,
+            accessedFieldsSetterVisitor,
+            gotoInfoVisitor,
+        };
 
-            var destructorVisitors = new List<IDestructorVisitor>
-            {
-                new DestructorInfoVisitor(),
-                linesOfCodeVisitor,
-                calledMethodSetterVisitor,
-                localFunctionsSetterClassVisitor,
-                attributeSetterVisitor,
-                localVariablesTypeSetterVisitor,
-                accessedFieldsSetterVisitor,
-                gotoInfoVisitor,
-            };
+        var destructorVisitors = new List<IDestructorVisitor>
+        {
+            new DestructorInfoVisitor(),
+            linesOfCodeVisitor,
+            calledMethodSetterVisitor,
+            localFunctionsSetterClassVisitor,
+            attributeSetterVisitor,
+            localVariablesTypeSetterVisitor,
+            accessedFieldsSetterVisitor,
+            gotoInfoVisitor,
+        };
 
-            var fieldVisitors = new List<ICSharpFieldVisitor>
-            {
-                new FieldInfoVisitor(),
-                attributeSetterVisitor,
-            };
+        var fieldVisitors = new List<ICSharpFieldVisitor>
+        {
+            new FieldInfoVisitor(),
+            attributeSetterVisitor,
+        };
 
-            var propertyAccessorsVisitors = new List<IMethodVisitor>
-            {
-                methodInfoVisitor,
-                calledMethodSetterVisitor,
-                attributeSetterVisitor,
-                linesOfCodeVisitor,
-                localFunctionsSetterClassVisitor,
-                localVariablesTypeSetterVisitor,
-                accessedFieldsSetterVisitor,
-                gotoInfoVisitor,
-            };
+        var propertyAccessorsVisitors = new List<IMethodVisitor>
+        {
+            methodInfoVisitor,
+            calledMethodSetterVisitor,
+            attributeSetterVisitor,
+            linesOfCodeVisitor,
+            localFunctionsSetterClassVisitor,
+            localVariablesTypeSetterVisitor,
+            accessedFieldsSetterVisitor,
+            gotoInfoVisitor,
+        };
 
-            var propertyVisitors = new List<ICSharpPropertyVisitor>
-            {
-                new PropertyInfoVisitor(),
-                new MethodAccessorSetterPropertyVisitor(propertyAccessorsVisitors),
-                linesOfCodeVisitor,
-                attributeSetterVisitor,
-            };
+        var propertyVisitors = new List<ICSharpPropertyVisitor>
+        {
+            new PropertyInfoVisitor(),
+            new MethodAccessorSetterPropertyVisitor(propertyAccessorsVisitors),
+            linesOfCodeVisitor,
+            attributeSetterVisitor,
+        };
 
-            var classVisitors = new List<ICSharpClassVisitor>
-            {
-                new BaseInfoClassVisitor(),
-                new BaseTypesClassVisitor(),
-                new MethodSetterClassVisitor(methodVisitors),
-                new ConstructorSetterClassVisitor(constructorVisitors),
-                new DestructorSetterClassVisitor(destructorVisitors),
-                new FieldSetterClassVisitor(fieldVisitors),
-                new PropertySetterClassVisitor(propertyVisitors),
-                new ImportsVisitor(),
-                linesOfCodeVisitor,
-                attributeSetterVisitor,
-                genericParameterSetterVisitor,
+        var classVisitors = new List<ICSharpClassVisitor>
+        {
+            new BaseInfoClassVisitor(),
+            new BaseTypesClassVisitor(),
+            new MethodSetterClassVisitor(methodVisitors),
+            new ConstructorSetterClassVisitor(constructorVisitors),
+            new DestructorSetterClassVisitor(destructorVisitors),
+            new FieldSetterClassVisitor(fieldVisitors),
+            new PropertySetterClassVisitor(propertyVisitors),
+            new ImportsVisitor(),
+            linesOfCodeVisitor,
+            attributeSetterVisitor,
+            genericParameterSetterVisitor,
 
-                // metrics visitor
-                new IsAbstractClassVisitor(),
-                new ExceptionsThrownRelationVisitor(relationMetricHolder),
-                new ObjectCreationRelationVisitor(relationMetricHolder),
-            };
+            // metrics visitor
+            new ExceptionsThrownRelationVisitor(relationMetricHolder),
+            new ObjectCreationRelationVisitor(relationMetricHolder),
+        };
 
-            var delegateVisitors = new List<ICSharpDelegateVisitor>
-            {
-                new BaseInfoDelegateVisitor(),
-                new ImportsVisitor(),
-                attributeSetterVisitor,
-                parameterSetterVisitor,
-                genericParameterSetterVisitor,
-            };
-            var compilationUnitVisitors = new List<ICSharpCompilationUnitVisitor>
-            {
-                new ClassSetterCompilationUnitVisitor(classVisitors),
-                new DelegateSetterCompilationUnitVisitor(delegateVisitors),
-                new ImportsVisitor(),
-                linesOfCodeVisitor,
-            };
+        var delegateVisitors = new List<ICSharpDelegateVisitor>
+        {
+            new BaseInfoDelegateVisitor(),
+            new ImportsVisitor(),
+            attributeSetterVisitor,
+            parameterSetterVisitor,
+            genericParameterSetterVisitor,
+        };
+        var compilationUnitVisitors = new List<ICSharpCompilationUnitVisitor>
+        {
+            new ClassSetterCompilationUnitVisitor(classVisitors),
+            new DelegateSetterCompilationUnitVisitor(delegateVisitors),
+            new ImportsVisitor(),
+            linesOfCodeVisitor,
+        };
 
-            var compositeVisitor = new CompositeVisitor();
+        var compositeVisitor = new CompositeVisitor();
 
-            foreach (var compilationUnitVisitor in compilationUnitVisitors)
-            {
-                compositeVisitor.Add(compilationUnitVisitor);
-            }
-
-            compositeVisitor.Accept(new LoggerSetterVisitor(logger));
-
-
-            return compositeVisitor;
+        foreach (var compilationUnitVisitor in compilationUnitVisitors)
+        {
+            compositeVisitor.Add(compilationUnitVisitor);
         }
+
+        compositeVisitor.Accept(new LoggerSetterVisitor(logger));
+
+
+        return compositeVisitor;
     }
 }
