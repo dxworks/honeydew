3122dd48bd58cf32f6c7cf2578a455fd7401f748
Victor Chircu <Victor.Chircu@gmail.com>
2022-09-13 10:30:42 +0300

    Add new line at the end of files

--- Honeydew/PostExtraction/Model/ComplexityMetrics/MetricsModelVisitor.cs
+++ Honeydew/PostExtraction/Model/ComplexityMetrics/MetricsModelVisitor.cs
@@ -8,17 +8,8 @@ public class MetricsModelVisitor : IModelVisitor<EntityModel>
     public void Visit(EntityModel entityModel)
     {
         if (entityModel is not ClassModel classModel) return;
-
-
-        //var atfd = classModel.ForeignData().Count();
-
-
+        
         //var nopa = classModel.Fields.Count(f => f.AccessModifier == AccessModifier.Public);
-
-        //woc += classModel.Properties.SelectMany(p => p.Accessors)
-        //    .Count(a => a.AccessModifier == AccessModifier.Public);
-        //nopa += classModel.Properties.Count(p => p.AccessModifier == AccessModifier.Public);
-
         var metrics = ClassMetrics.For(classModel);
         metrics.WeightOfAClass = WeightOfAClass.Value(classModel);
         metrics.AccessToForeignDataForType = AccessToForeignDataForType.Value(classModel);
