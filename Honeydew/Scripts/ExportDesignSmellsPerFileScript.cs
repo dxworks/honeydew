7271fa7d6af793365d05e45d83babbf48102ffb9
Victor Chircu <Victor.Chircu@gmail.com>
2022-09-21 17:58:44 +0300

    Add issue repro

--- Honeydew/Scripts/ExportDesignSmellsPerFileScript.cs
+++ Honeydew/Scripts/ExportDesignSmellsPerFileScript.cs
@@ -22,6 +22,31 @@ public class ExportDesignSmellsPerFileScript : Script
         var outputName = VerifyArgument<string>(arguments, "designSmellsOutputName")!;
         var repositoryModel = VerifyArgument<RepositoryModel>(arguments, "referenceRepositoryModel")!;
 
+        // #1 The base type of CategoryRepository is BaseRepository<EntityT, IdT>, which should be marked as internal
+        var entityModel = repositoryModel.GetEnumerable().SingleOrDefault(e => e.Name.Contains("NetCoreCMS.EasyNews.Repositories.CategoryRepository"));
+        var classModel = entityModel as ClassModel;
+        var baseType = classModel.BaseTypes.SingleOrDefault();
+        if (!baseType.IsInternal)
+        {
+            //should be internal
+            var entityIsInternal = baseType.Entity.IsInternal; // also false
+        }
+
+        // #2 The NccRazorPage.FireEvent method uses a _mediator instance variable. But the FieldAccess.Field.Entity is different than the method's Entity.
+        entityModel = repositoryModel.GetEnumerable().SingleOrDefault(e => e.Name.Contains("NccRazorPage"));
+        classModel = entityModel as ClassModel;
+        var methodModel = classModel.Methods.SingleOrDefault(m => m.Name.Contains("FireEvent"));
+        var fieldAccess = methodModel.FieldAccesses.FirstOrDefault(fa => fa.Field.Name == "_mediator");
+
+        if (methodModel.Entity != fieldAccess.Field.Entity)
+        {
+            // they should be the same, as the field is defined in the same class as the method
+            var classModelName = classModel.Name; //NetCoreCMS.Framework.Core.Mvc.Views.NccRazorPage<TModel>
+            var accessEntityType = fieldAccess.AccessEntityType.Name; //NetCoreCMS.Framework.Core.Mvc.Views.NccRazorPage<TModel>
+            var fieldAccessFieldEntity = fieldAccess.Field.Entity.Name; //NetCoreCMS.Framework.Core.Mvc.Views.NccRazorPage
+            var methodEntity = methodModel.Entity.Name; //NetCoreCMS.Framework.Core.Mvc.Views.NccRazorPage<TModel>
+        }
+
         var designSmells = new DesignSmellsDetectionRunner(_logger).Detect(repositoryModel).OrderBy(ds => ds.SourceFile);
 
         DesignSmellsJsonWriter.Export(_modelExporter, designSmells, Path.Combine(outputPath, outputName));
