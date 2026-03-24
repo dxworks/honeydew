99bf9b23e02a452e2cd478826343426b6be987c6
Victor Chircu <victor.chircu@gmail.com>
2026-03-24 15:11:46 +0200

    Update gh actions to .NET 10. Remove  reference to DxWorks.ScriptBee.Plugin.Api

--- DxWorks.ScriptBee.Plugins.Honeydew/Models/ReferenceEntity.cs
+++ DxWorks.ScriptBee.Plugins.Honeydew/Models/ReferenceEntity.cs
@@ -1,7 +1,22 @@
-using DxWorks.ScriptBee.Plugin.Api;
-
 namespace DxWorks.ScriptBee.Plugins.Honeydew.Models;
 
-public abstract class ReferenceEntity : ScriptBeeModel
+public abstract class ReferenceEntity
 {
+    private readonly Dictionary<string, object?> _properties = new();
+
+    public object? this[string propertyName]
+    {
+        get => _properties[propertyName];
+        set => _properties[propertyName] = value;
+    }
+
+    public IReadOnlyDictionary<string, object?> GetProperties()
+    {
+        return _properties;
+    }
+
+    public bool HasProperty(string propertyName)
+    {
+        return _properties.ContainsKey(propertyName);
+    }
 }
